f74dee65455fdd2973731a693564da6d3e1aca75
Luccas Lima <85250416+LimaXII@users.noreply.github.com>
2021-09-01 19:24:28 -0300

    Removi e acrescentei alguns comentários

--- lab1.py
+++ lab1.py
@@ -2,7 +2,8 @@
 #Nomes: Luccas da Silva Lima e Bruno Longo Farina
 #Turma: B
 
-import time #biblioteca necessaria para cronometrar o tempo.
+#biblioteca necessaria para cronometrar o tempo.
+import time 
 
 #vai criar uma lista onde vai ser posto o texto antes de ser escrito no arquivo
 textinho = []
@@ -21,7 +22,7 @@ def definir_seq(seq):
 #Funcao shell_sort. Recebe como parametro um vetor, o seu tamanho e uma sequencia.
 def shell_sort(array,n,seq):   
     
-    #exercicio 1:
+    #Exercicio 1:
     #escreve no textinho o array intacto e qual sequencia está sendo utilizada (ver se n é melhor fazer isso dentro da main ao inves da função)
     
     textinho.append(str(array))
@@ -69,20 +70,14 @@ def shell_sort(array,n,seq):
             
         #Caso seq[i+1] nao for >= n, incrementa i.
         else:
-            i = i + 1
-    
-              
-                      
+            i = i + 1                  
 
 
 #Funcao shell_sort. Recebe como parametro um vetor, o seu tamanho e uma sequencia.
 def shell_sort2(array,n,seq):   
     
    
-    #exercicio 2:
-    #!!! Talvez tentar colcoar isso na main ou em outra funcao, nao sei qual seria a melhor escolha.
-    #O programa funciona perfeitamente desse jeito, so fica meio baguncado 
-    
+    #Exercicio 2:    
     inicio = time.time() #variavel que ira cronometrar o tempo de execucao.    
     textinho2.append(definir_seq(seq))
     textinho2.append(', ')    
@@ -115,13 +110,12 @@ def shell_sort2(array,n,seq):
                 #decrementa i, para retroceder uma posicao no vetor das sequencias.                
                 i = i - 1
             #Caso o ordenamento tenha acontecido, i = 20 para interromper o loop no while.
-            i = 20
+            i = 20                    
             
-            #!!! Talvez tentar colcoar isso na main ou em outra funcao, nao sei qual seria a melhor escolha.
-            #O programa funciona perfeitamente desse jeito, so fica meio baguncado            
-            
+            #Calcula o tempo decorrido durante a execucao da funcao.
             fim = time.time()
             tempo = (fim - inicio)
+            #Salva o tempo na string textinho2.
             textinho2.append(str(tempo))
             textinho2.append('\n')    
 
@@ -141,8 +135,8 @@ ciura = [1,4,10,23,57,132,301,701,1577,3548,7983,17961,40412,90927,204585,460316
 with open('entrada1.txt','r') as f: 
     tamanho = [int(r.split()[0]) for r in f]
 
-#abre o arquivo de novo (n sei pq fechou)
-f = open('entrada1.txt','r') #C:\\Users\\bruno\\OneDrive\\Documentos\\GitHub\\Lab1-Shell_sort\\
+#Abre o arquivo novamente. 
+f = open('entrada1.txt','r') 
 
 #pra cada linha ele armazena os valores numa string
 for i in tamanho:
@@ -158,10 +152,10 @@ for i in tamanho:
     #e faz o shell sort dela
     shell_sort(res1,i,shell)
     shell_sort(res2,i,knuth)
-    shell_sort(res3,i,ciura)
-   
-    #arquivo.write(str(res)+'\n')
-arquivo = open('saida1.txt', 'w') #C:\\Users\\bruno\\OneDrive\\Documentos\\GitHub\\Lab1-Shell_sort\\
+    shell_sort(res3,i,ciura)    
+
+#Cria o segundo arquivo de saida, contendo os resultados do exercicio 1.
+arquivo = open('saida1.txt', 'w') 
 j = 0
 for i in textinho:
     arquivo.write(textinho[j])
@@ -169,31 +163,35 @@ for i in textinho:
 arquivo.close()
 f.close()
 
-with open('entrada2.txt','r') as f: #C:\\Users\\bruno\\OneDrive\\Documentos\\GitHub\\Lab1-Shell_sort\\
-    tamanho = [int(r.split()[0]) for r in f]
 #Exercicio 2:
-#tamanho = [100, 1000, 10000, 100000, 1000000]
-f = open('entrada2.txt', 'r') #C:\\Users\\bruno\\OneDrive\\Documentos\\GitHub\\fafafa\\fafasfa\\
+#Abre o arquivo entrada2.txt e armazenha na variavel tamanho, os tamanhos de todos os vetores presentes no arquivo.
+with open('entrada2.txt','r') as f: 
+    tamanho = [int(r.split()[0]) for r in f]
+
+#Abre o arquivo entrada2.txt e le os vetores presentes nele.
+f = open('entrada2.txt', 'r') 
 for i in tamanho:
     data = f.readline()
     #converte a string em uma lista de inteiros
     res1 = [int(i) for i in data.split()]
     res2 = [int(i) for i in data.split()]
     res3 = [int(i) for i in data.split()]
-    #remove o primeiro valor, que era o tamanho 
+    #remove o primeiro valor dos vetores (este valor informava o tamanho de cada vetor). 
     del res1[0] 
     del res2[0]
     del res3[0]   
 
-    #e faz o shell sort dela
+    #Faz o shell sort dela
     shell_sort2(res1,i,shell)  
     shell_sort2(res2,i,knuth)
     shell_sort2(res3,i,ciura)
 
-arquivo = open('saida2.txt', 'w') #C:\\Users\\bruno\\OneDrive\\Documentos\\GitHub\\fafafa\\fafasfa\\
+#Cria o segundo arquivo de saida, contendo os resultados do exercicio 2.
+arquivo = open('saida2.txt', 'w') 
 j = 0
 for i in textinho2:
     arquivo.write(textinho2[j])
     j=j+1
+#Fecha o arquivo.
 arquivo.close()
 f.close()
