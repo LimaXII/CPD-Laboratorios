473a419082bd5460e4602ccc4fa3be92f35e471e
Luccas Lima <85250416+LimaXII@users.noreply.github.com>
2021-09-28 17:24:11 -0300

    Lab3 e primeiros passos

---
+++ Laboratório 3/Radixsort.py
@@ -0,0 +1,35 @@
+#Laboratorio 3 CPD - Radix sort 
+#Nomes: Luccas da Silva Lima e Bruno Longo Farina
+#Turma: B
+
+#Considere apenas as palavras contendo pelo menos 4 caracteres e apenas 
+#letras do alfabeto ingles, isto e, ignore numerais e simbolos.
+
+
+#variavel que vai armazenar o texto.
+#O texto vai ser um vetor e cada palavra sera um nodo do vetor.
+texto_completo = []
+texto_real = []
+
+def deletest(texto):
+    for i in range(0, len(texto)):
+        if (len(texto[i]) >= 4):
+            texto_real.append(texto[i])
+
+#Abre e le o arquivo inteiro.
+with open('frankestein_clean.txt','r') as f: 
+    data = f.readline()    
+    texto = [str(i) for i in data.split()] 
+
+#Chama a funcao para deletar todas as palavras com menos que 4 letras do texto.
+deletest(texto)
+
+#So criei um arquivo para testar se o vetor texto realmente tava com o texto certinho.
+arquivo = open('escrita_teste.txt', 'w')
+j = 0
+i = 0
+for i in texto_real:
+    arquivo.write(texto_real[j])
+    arquivo.write(' ')
+    j=j+1
+arquivo.close()
